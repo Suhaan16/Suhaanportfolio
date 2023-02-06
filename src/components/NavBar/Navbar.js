8eda130c2b2f1ee843ebfb2b0a7ac2a875957225
Chethan B N <47515084+Chethan30@users.noreply.github.com>
2023-02-05 21:20:55 -0600

    no ideas yet, but some improvements

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -6,19 +6,19 @@ export default function Navbar() {
   return (
     <div className="nav-container">
       <div>
-        <img className="nav-logo" src={logoWhite} alt="logo-white" />
+        <img className="nav-logo link" src={logoWhite} alt="logo-white" />
       </div>
       <div className="nav-links">
-        <a href="http://" rel="noopener noreferrer">
+        <a className="sec link" href="http://" rel="noopener noreferrer">
           About
         </a>
-        <a href="http://" rel="noopener noreferrer">
+        <a className="sec link" href="http://" rel="noopener noreferrer">
           Work
         </a>
-        <a href="http://" rel="noopener noreferrer">
+        <a className="sec link" href="http://" rel="noopener noreferrer">
           Servcies
         </a>
-        <a href="http://" rel="noopener noreferrer">
+        <a className="sec link" href="http://" rel="noopener noreferrer">
           Contact
         </a>
       </div>
